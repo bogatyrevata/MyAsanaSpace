1b5fc14fa21e58580426f0fa8a1756b0edc8acb1
taisiia bogatyreva <bogatyrevata@gmail.com>
2025-11-13 00:49:31 -0300

    Компоненты страниц

---
+++ frontend/src/components/HomePage.jsx
@@ -0,0 +1,20 @@
+
+function HomePage() {
+  return (
+    <section className="text-center mb-20">
+        <div className="relative inline-block">
+            <div className="absolute inset-0 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full pulse-ring" />
+                <h2 className="relative text-4xl md:text-6xl font-bold bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent mb-6">
+                    Пробуди свою энергию!
+                </h2>
+            </div>
+        <p className="text-xl text-gray-700 max-w-3xl mx-auto mb-8">
+        Откройте для себя мир йоги с нашими интенсивными и вдохновляющими практиками.
+        Трансформируйте свое тело и разум уже сегодня!
+        </p>
+
+    </section>
+  );
+}
+
+export default HomePage;
